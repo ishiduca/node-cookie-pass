b3813cc4eb9993d36a10f2593e115a1641577869
ishiduca <ishiduca@gmail.com>
2014-05-26 10:05:44 +0900

    bug fix

--- t/00_load.js
+++ t/00_load.js
@@ -3,13 +3,13 @@ var cookiepass
 
 test('var cookiepass = require("cookie-pass")', function (t) {
     t.equal(typeof (cookiepass = require('..')), 'function', 'cookiepass is function')
-	t.end()
+    t.end()
 })
 
 test('var cookie = cookiepass(http.incomingMessage)', function (t) {
-	var cookie = cookiepass()
-	t.ok(cookie, 'cookie')
-	t.equal(typeof cookie.pass, 'function', 'cookie.pass is function')
-	t.equal(typeof cookie.merge, 'function', 'cookie.merge is function')
-	t.end()
+    var cookie = cookiepass()
+    t.ok(cookie, 'cookie')
+    t.equal(typeof cookie.pass, 'function', 'cookie.pass is function')
+    t.equal(typeof cookie.merge, 'function', 'cookie.merge is function')
+    t.end()
 })
